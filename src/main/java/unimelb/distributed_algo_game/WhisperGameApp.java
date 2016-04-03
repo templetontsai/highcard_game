0099170a04ac725cb54baa79878fc41f5de7fb4c
Templeton Tsai <qpzmy701117@gmail.com>
2016-04-03 22:05:31 +1000

    Init Whisper Game Project

---
+++ src/main/java/unimelb/distributed_algo_game/WhisperGameApp.java
@@ -0,0 +1,10 @@
+package unimelb.distributed_algo_game;
+
+public class WhisperGameApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println( "Hello World!" );
+	}
+
+}
