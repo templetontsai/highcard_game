790b3887d688f5b16313b308f1c9ff82f676198f
Templeton Tsai <qpzmy701117@gmail.com>
2016-04-10 01:03:51 +1000

    Refactoring the name of game from WhisperGameApp to HighCardGameApp

--- src/main/java/unimelb/distributed_algo_game/HighCardGameApp.java
+++ src/main/java/unimelb/distributed_algo_game/HighCardGameApp.java
@@ -1,6 +1,6 @@
 package unimelb.distributed_algo_game;
 
-public class WhisperGameApp {
+public class HighCardGameApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
