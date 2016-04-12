92472b2b8890dc352573e841914a9bbbb28aa26c
Lupiya Mujala <lupiyamujala@gmail.com>
2016-04-12 23:00:54 +1000

    Gameplay Changes
    Main thread in main method that runs a simple match to determine winner.

--- src/main/java/unimelb/distributed_algo_game/HighCardGameApp.java
+++ src/main/java/unimelb/distributed_algo_game/HighCardGameApp.java
@@ -3,12 +3,15 @@ package unimelb.distributed_algo_game;
 
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 import player.AIPlayer;
 import player.HumanPlayer;
 import player.Player;
 import unimelb.distributed_algo_game.pokers.Card;
 import unimelb.distributed_algo_game.pokers.Deck;
+import unimelb.distributed_algo_game.pokers.Util;
 
 public class HighCardGameApp {
 
@@ -22,16 +25,55 @@ public class HighCardGameApp {
 		
 		Player p1 = new HumanPlayer("Templeton", 1);
 		Player p2 = new AIPlayer("AI", 2);
-		Thread t1 = new Thread(p1);
-		Thread t2 = new Thread(p2);
+		//Thread t1 = new Thread(p1);
+		//Thread t2 = new Thread(p2);
 
-		p2.setCards(deck.getCards(1));
-		p1.setCards(deck.getDeck());
+		Random random = new Random();
+		//p1.setCards(deck.getDeck());
 		
-		t1.start();
+		ArrayList<Player> players = new ArrayList<Player>();
+		players.add(p1);
+		players.add(p2);
 		
-		
-	//	t2.start();
+		//t1.start();
+		//t2.start();
+		boolean isFinished = false;
+		boolean roundDone = false;
+		Util deckComparison = new Util();
+		Scanner scanner = null;
+		while(!isFinished){
+		    int choice = random.nextInt(51) + 0;
+		    p2.selectFromDeck(deck.getCardFromDeck(choice));
+			while(!roundDone){
+				System.out.println("Please select from the deck between 1-52");
+				scanner = new Scanner(System.in);
+				int option = scanner.nextInt()-1;
+				if(option==choice){
+					System.out.println("Other player already selected that card.");
+				}else if(option>=0 && option<52){
+					roundDone = true;
+					p1.selectFromDeck(deck.getCardFromDeck(option));
+				}else{
+					System.out.println("Invalid selection.");
+				}
+			}
+			System.out.println(deckComparison.highCard(players));
+			boolean newMatch = false;
+			while(!newMatch){
+			   System.out.println("Do you want to continue? [Y/N]");
+			   scanner = new Scanner(System.in);
+			   String option = scanner.nextLine();
+			   if(option.equalsIgnoreCase("y")){
+				  roundDone = false;
+			      newMatch = true;
+				  deck.shuffle();
+			   }else if(option.equalsIgnoreCase("n")){
+				  isFinished = true;
+			      newMatch = true;
+			   }else
+				  System.out.println("Invalid selection.");
+			}
+		}
 	
 	}
 
