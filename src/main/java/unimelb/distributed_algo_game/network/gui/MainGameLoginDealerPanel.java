6304ea09fae4208a7ac5140a3f9164751f2e2b53
Templeton Tsai <qpzmy701117@gmail.com>
2016-05-14 22:14:00 +1000

    Removing hardcoded fixed ip address and port number

--- src/main/java/unimelb/distributed_algo_game/network/gui/MainGameLoginDealerPanel.java
+++ src/main/java/unimelb/distributed_algo_game/network/gui/MainGameLoginDealerPanel.java
@@ -94,12 +94,12 @@ public class MainGameLoginDealerPanel extends JPanel {
 		public void actionPerformed(ActionEvent action) {
 			// TODO get ip and port from textfield and set init server
 			// socket
-/*
-			String ipAddress = ipTextField.getText();
-			String port = portTextField.getText();*/
 
-			String ipAddress = "localhost";
-			String port = "5000";
+			String ipAddress = ipTextField.getText();
+			String port = portTextField.getText();
+
+			/*String ipAddress = "localhost";
+			String port = "5000";*/
 			if (!ipAddress.equals("") && !port.equals("")) {
 				System.out.println(ipAddress + "-" + port);
 				System.out.println("Dealer/Node0 Starts the game");
